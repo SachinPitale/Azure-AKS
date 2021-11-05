ff7d9d6bb75c1297955b5ba768beff45d5197ca2
SachinPitale <sachinpitale22@gmail.com>
2021-11-05 17:28:12 +0530

    AKS Terraform

--- 23-Azure-AKS-Terraform/23-04-Create-AKS-NodePools-using-Terraform/terraform-manifests-aks/01-main.tf
+++ 23-Azure-AKS-Terraform/23-04-Create-AKS-NodePools-using-Terraform/terraform-manifests-aks/01-main.tf
@@ -28,8 +28,8 @@ terraform {
 
 # Terraform State Storage to Azure Storage Container
   backend "azurerm" {
-    resource_group_name   = "terraform-storage-rg"
-    storage_account_name  = "terraformstatexlrwdrzs"
+    resource_group_name   = "sachinsg"
+    storage_account_name  = "sachinpitale123456"
     container_name        = "tfstatefiles"
     key                   = "dev.terraform.tfstate"
   }  
